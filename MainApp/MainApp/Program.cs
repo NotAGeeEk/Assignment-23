d349c670e4717cd895743faa9a15356f9cf67977
Amans <notshashank10@gmail.com>
2023-08-04 15:23:13 +0530

    first commit

---
+++ MainApp/MainApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //To  Test UserManagementModule
+            UserManagementModule.AddUser("Aman Saxena");
+            UserManagementModule.DeleteUser("Aman Saxena");
+
+            // To Test FileProcessingModule
+            FileProcessingModule.ProcessFile("example.txt");
+
+            // To Test Logging from MySharedLibrary
+            MySharedLibrary.UtilityMethods.Log("This is a log message Please cCheck it.");
+
+            //To Test Encryption from MySharedLibrary
+            string data = "Original Dataa";
+            string encryptedData = MySharedLibrary.UtilityMethods.EncryptData(data);
+            Console.WriteLine($"Encrypted Data: {encryptedData}");
+        }
+    }
+}
